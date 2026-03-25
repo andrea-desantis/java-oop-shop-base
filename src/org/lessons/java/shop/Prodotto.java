2509f6ba87a1f15694ebc4abad66043f2b5dffd0
andrea-desantis <andrea.desantis290@gmail.com>
2026-03-25 19:48:11 +0100

    first commit

---
+++ src/org/lessons/java/shop/Prodotto.java
@@ -0,0 +1,15 @@
+package org.lessons.java.shop;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+
+public class Prodotto {
+    public int codice;
+    public String nome;
+    public String descrizione;
+    public BigDecimal prezzo;
+    public BigDecimal iva;
+
+    
+}

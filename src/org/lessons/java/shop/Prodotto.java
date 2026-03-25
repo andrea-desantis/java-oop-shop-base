f6ae1037ddd374a92d94553b831082cdcfb78985
andrea-desantis <andrea.desantis290@gmail.com>
2026-03-25 20:33:06 +0100

    feat: prezzoIva

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -3,6 +3,8 @@ package org.lessons.java.shop;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+import java.util.Random;
+
 
 public class Prodotto {
     public int codice;
@@ -11,5 +13,26 @@ public class Prodotto {
     public BigDecimal prezzo;
     public BigDecimal iva;
 
-    
+    public Prodotto (String nome, String descrizione, BigDecimal prezzo, BigDecimal iva){
+        Random rand = new Random();
+        this.codice = rand.nextInt(99999);
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.prezzo = prezzo;
+        this.iva = iva;
+    }
+
+    public BigDecimal getPrezzoBase(){
+        return this.prezzo;
+    }
+
+    public BigDecimal getPrezzoConIva(){
+        
+        return prezzo.add(prezzo.multiply(iva)).setScale(2, RoundingMode.DOWN);
+        // aggiungi al prezzo (che è senza iva, netto)
+        // il prezzo dell'iva (per esempio prezzo base*iva, 
+        // che sarà un decimale tipo 0.22, ovvero il 22%).
+        // poi arrotonda a 2 decimali, per difetto(verso il basso/DOWN)
+
+    }
 }
